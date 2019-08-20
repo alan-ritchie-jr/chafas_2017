ab0484ab312df269bdadb5e3bdf1c1dd9af60167
ritch173 <ritch173@umn.edu>
2019-08-20 16:15:41 -0500

    20Aug2019: Plot code script-- Cleaned and modified code for publication mostly size adjustments.

--- code/plot_code_July2019.R
+++ code/plot_code_July2019.R
@@ -2,6 +2,8 @@ require(ggplot2)
 #load tidyverse
 library(tidyverse)
 library(sjPlot)
+install.packages("dichromat")
+library(dichromat)
 ###figure 1: hypothesized PL- lansdcape relationship
 
 #create desired function to plot
@@ -11,31 +13,31 @@ hand_poll(c(5,4,3,2,1))#test
 #or simulate data
 # A different data set
 df1 <- data.frame(
-  Treatment= factor(c("Supplemental Pollination","Supplemental Pollination","Supplemental Pollination","Supplemental Pollination","Supplemental Pollination", "Open Pollination", "Open Pollination","Open Pollination","Open Pollination","Open Pollination")),
+  Treatment= factor(c("Supplemental Pollination","Supplemental Pollination","Supplemental Pollination","Supplemental Pollination","Supplemental Pollination", "Ambient Pollination", "Ambient Pollination","Ambient Pollination","Ambient Pollination","Ambient Pollination")),
   landscape = c("20%","30%","40%","50%","60%","20%","30%","40%","50%","60%"),
   seedset = c(5,5,5,5,5, 5,4,3,2,1))
 df1
 
-
+df1$linetype= ifelse(df1$Treatment=="Supplemental Pollination", "solid", "dashed")
 #create legend
 test.labels<-c("Supplemental Pollinated","Open Pollinated")
 #create plot
 #f <- ggplot(data.frame(x = c(0,0.2,0.4,0.6,0.8,1.0), y=c(0,1,2,3,4,5)), aes(x))
-f<- ggplot(data=df1, aes(landscape, seedset, group=Treatment, shape=Treatment))
-#f + stat_function(fun = HP.density, size=2) +stat_function(fun=Open Pollination.density, size=1.5, linetype=2)+ 
-f+ geom_line(aes(linetype=Treatment), size=1.5)+
+ggplot(data=df1, aes(landscape, seedset, group=Treatment))+
+  geom_line(aes(linetype=Treatment),size=2)+
   xlab("% Agriculture Surrounding Restoration")+
-  ylab("Seed Set")  + ylim(0.5,7)+ ggtitle("Fig.1: Predicted Relationship Between Seed Set and \n Surrounding Agricultural Land-Use")+
+  ylab("Seed Set")  + ylim(0.5,7)+ ggtitle("Fig.1: Hypothesized Relationship Between Seed Set and \n Surrounding Agricultural Land Use")+
   theme_bw() +
-  theme(axis.text=element_text(size=14,color="black"),
-        axis.line = element_line(colour = "black"),
-        axis.title=element_text(size=16),
-        title=element_text(size=18),
+  scale_linetype_manual(values=c("dashed", "solid"))+
+  theme(axis.text=element_text(size=34,color="black"),
+        axis.line = element_line(colour = "black", size=2),
+        axis.title=element_text(size=38),
+        title=element_text(size=40),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),legend.position = "none")
 
-#### figure S1 -- gradient
+ #### figure S1 -- gradient
 
 #load in seed data
 
@@ -50,9 +52,9 @@ seed_land$site<-recode(seed_land$site,"braaten"="Site 1", "nelson"="Site 2", "ru
 
 seed_land%>%ggplot(aes(fct_reorder(site, prop.c),prop.c))+geom_point(size=8)+
   labs(x="Site" ,y="% Agriculture")+
-  ggtitle("Study System Agricultural Land-use Gradient")+theme_bw()+
-  theme(axis.text=element_text(size=19,color="black"),
-        axis.title=element_text(size=19),title=element_text(size=19),
+  ggtitle("Study System Agricultural Land Use Gradient")+theme_bw()+
+  theme(axis.text=element_text(size=36,color="black"),
+        axis.title=element_text(size=38),title=element_text(size=38),
         axis.line = element_line(colour = "black"),panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),legend.position = "none") # change legend text size
@@ -131,23 +133,12 @@ mn_map_zoom <- ggplot(data = mn_counties, mapping = aes(x = long, y = lat, group
 ### zoom map
 mn_map_zoom
 
-###Fig 2: 
-#mean seeds produced per fruits collected for a plant 
-seed_land%>% 
-  mutate(trmnt = fct_recode(trmnt, "Supplemental Pollination" = "hp", "Open Pollination"="op"))%>%
-  group_by(trmnt, ID)%>%
-  summarize(mean_seed=mean(total.seeds))%>%
-  ggplot(aes(trmnt ,mean_seed ))+geom_boxplot()+ theme_bw()+
-  labs(x="Treatment" ,y="Seeds Produced")+
-  ggtitle("Fig. 2: Mean Seeds Produced per Treated Fruit")+
-  theme(axis.text=element_text(size=19,color="black"),
-        axis.title=element_text(size=19),title=element_text(size=19),
-        axis.line = element_line(colour = "black"),panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),legend.position = "none")
+############################
+#Fig 2: LMM predicted response vs raw data
+
 ##########################
 
-####note: model object is generated in model script
+####note!!!: model object is generated in model script! Run it first!
 
 #########################
 #Plotting predicted effect and actual data
@@ -162,9 +153,10 @@ seed_land%>%
 
 ### get_model_data + ggplot version
 theme_set(theme_bw())
+theme_set(theme_classic())
 #run get_model_data to extract ggplot usable output
 s<-get_model_data(seed_mean_mod_TMB,ci.lvl= .95, type="pred",terms=c("prop.c","trmnt"), 
-                  pred.type="re", colors= "bw") 
+                  pred.type="re") 
 s<-s%>%mutate(group = fct_recode(group, 
                                  "Supplemental Pollination" = "SP","Open Pollination"="OP"))
 ### make separate dataframes for CI for plot
@@ -173,23 +165,37 @@ sop<-s%>%filter(group=="Open Pollination")
 
 ###make plot for seed model
 ggplot(data=s, aes(x, predicted),linetype=group)+
-  geom_line(aes(linetype=group),size=1)+
-  geom_point(data=plt_nr,aes(prop.c, (seeds/frt),shape=trmnt),position="jitter",
-             inherit.aes = FALSE,size=3)+
+  geom_line(aes(linetype=group),size=3)+
+  geom_point(data=plt_nr,aes(prop.c, (seeds/frt),shape=trmnt),position="jitter", inherit.aes = FALSE,size=7)+
   geom_ribbon(data=shp,aes(x=x,ymin=conf.low, ymax=conf.high),alpha = 0.3,inherit.aes = FALSE)+
   geom_ribbon(data=sop,aes(x=x,ymin=conf.low, ymax=conf.high),alpha = 0.3,inherit.aes = FALSE)+
   labs(shape="Treatment",linetype="Predicted Response")+
-  xlab("% Agriculture")+ylab("Mean Seeds/Treated Fruit per Plant ")+
-  ggtitle("Fig. 3: Effect of % Agriculture on Seed Set",
-          subtitle="Predicted Response vs. Data")+
-  theme(axis.text=element_text(size=14,color="black"),
-        axis.title=element_text(size=16),
-        title=element_text(size=18),
-        legend.text=element_text(size=14),
-        axis.line = element_line(colour = "black"),panel.border = element_blank(),
+  xlab("% Agriculture Surrounding Restoration")+ylab("Mean Seeds per Treated Fruit")+
+  ggtitle("Fig. 2: Effect of Surrounding Agriculture on Seed Set",
+          subtitle=" LMM Predicted Response vs. Data")+
+  theme(axis.text=element_text(size=34,color="black"),
+        axis.title=element_text(size=38),
+        title=element_text(size=40),
+        legend.text=element_text(size=38),
+        axis.line = element_line(colour = "black",size=2),panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),legend.position = "none")
 
-#Note: I haven't figured out how to facet both the data and the predicted values by treatment
 
-  
+###
+###Fig 3: 
+#mean seeds produced per fruits collected for a plant 
+seed_land%>% 
+  mutate(trmnt = fct_recode(trmnt, "Supplemental Pollination" = "hp", "Open Pollination"="op"))%>%
+  group_by(trmnt, ID)%>%
+  summarize(mean_seed=mean(total.seeds))%>%
+  ggplot(aes(trmnt ,mean_seed))+geom_boxplot(lwd=2,outlier.size=5)+ theme_bw()+
+  labs(x="Treatment" ,y="Mean Seeds per Treated Fruit")+
+  ggtitle("Fig. 3: Mean Seed Set per Treatment")+
+  theme(axis.text=element_text(size=38,color="black"),
+        axis.title=element_text(size=40),title=element_text(size=40),
+        axis.line = element_line(colour = "black", size=2),panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),legend.position = "none")
+
+
